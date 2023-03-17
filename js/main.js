3904e874ee8be699adfaf8b6794a1c74e08fe85f
Kyle Knutson <knutson.kyle86@gmail.com>
2023-03-16 21:57:18 -0700

    Update click function
    Updated Rooms.js, Can find items by clicking within item coordinates (collision box),
    Can display descriptions to Message Box, updated some function names

--- js/main.js
+++ js/main.js
@@ -5,110 +5,106 @@ const btn_take = document.getElementById("btn-take");
 const btn_use = document.getElementById("btn-use");
 const btn_drop = document.getElementById("btn-drop");
 const btn_speak = document.getElementById("btn-speak");
-
 const btn_yes = document.getElementById("btn-yes");
 const btn_no = document.getElementById("btn-no");
 
-const FRAMES_PER_SECOND = 5; 
-console.log("FRAMES_PER_SECOND = ", FRAMES_PER_SECOND); 
+const FRAMES_PER_SECOND = 30; 
 
-
-let currentRoom = "room01";
+let currentRoom = "Room01";
 let currentAction = null;
+let listOfItemCoordinates = [];
+let listOfItemDescriptions = [];
 
 
+function checkThisRoom (whichRoom, mouseX, mouseY) {
+    let listOfAllRoomItems = Object.keys(whichRoom.allItems);
+    let itemIsClicked = false;
 
-function checkForSceneObject(){
-    // compare mouse position with scene object positions.
-}
+    dictionaryOfRoomItems = whichRoom.allItems;
+    listOfAllRoomItems.forEach(item => { listOfItemCoordinates.push(dictionaryOfRoomItems[item].coords) });
+    listOfAllRoomItems.forEach(item => { listOfItemDescriptions.push(dictionaryOfRoomItems[item].description) });
 
-
-function displayMousePos(e) {
-    console.log(e);
-    let mouseX = e.offsetX;
-    let mouseY = e.offsetY;
-    document.getElementById("mouse-xy").innerHTML = ("X: "+mouseX+", Y: "+mouseY+" ");
-
-// check Action being used
-// perform Action based on clicked entity/object
-    let entityIsClicked = false
-
-    for( let i = 0; entityIsClicked == false && i < entityPositionList.length ; i++ ) {
-        if(mouseX >= entityPositionList[i].coords[0] && 
-            mouseX <= entityPositionList[i].coords[2] &&
-            mouseY >= entityPositionList[i].coords[1] &&
-            mouseY <= entityPositionList[i].coords[3] ) 
+    for( let i = 0; itemIsClicked == false && i < listOfItemCoordinates.length ; i++ ) {
+        if(mouseX >=  listOfItemCoordinates[i][0] && 
+            mouseX <= listOfItemCoordinates[i][2] &&
+            mouseY >= listOfItemCoordinates[i][1] &&
+            mouseY <= listOfItemCoordinates[i][3] ) 
         {
-
-            entityIsClicked = true;
-            console.log(entityPositionList[i].entity)
-            document.getElementById("item-clicked").innerHTML = entityPositionList[i].name;
-
+            itemIsClicked = true;
+            document.getElementById("message-box").innerHTML = listOfItemDescriptions[i];
             if (currentAction === "move" ) {
                currentRoom = "room02"
                currentAction = null;
             }
-
-
-        } else { document.getElementById("item-clicked").innerHTML = "Nothing Interesting...";}
+        } else { document.getElementById("message-box").innerHTML = "Quit joking around, there's nothing interesting here..." }
     }
 }
 
+function checkForClickableItems(e){
+    let mouseX = e.offsetX;
+    let mouseY = e.offsetY;
+    let dictionaryOfRoomItems;
+    let listAllRoomItems;
 
+    displayMousePos(e, mouseX, mouseY);
+    if(currentRoom === 'Room01'){ checkThisRoom(Room01, mouseX, mouseY) };
+    if(currentRoom === 'Room02'){ checkThisRoom(Room02, mouseX, mouseY) };
+}
 
-function setAction(action = null) {
+function displayMousePos(e, mouseX, mouseY) {
+    console.log(e);
+    document.getElementById("mouse-xy").innerHTML = ("X: "+mouseX+", Y: "+mouseY+" ");
+}
+
+function setCurrentAction(action = null) {
     currentAction = action;
     console.log("currentAction = ",currentAction);
     document.getElementById("current-action").innerHTML = action.toUpperCase();
 }
 
-
-
-
 window.onload = function () {
     canvas = document.getElementById('game-canvas');
     context = canvas.getContext('2d');
-  
-    canvas.addEventListener("click", function(e) { displayMousePos(e) });
 
-    btn_move.addEventListener("click", function(){ setAction("move") });
-    btn_look.addEventListener("click", function(){ setAction("look") });
-    btn_take.addEventListener("click", function(){ setAction("take") })
-    btn_use.addEventListener("click", function(){ setAction("use") })
-    btn_drop.addEventListener("click", function(){ setAction("drop") })
-    btn_speak.addEventListener("click", function(){ setAction("speak") })
+    canvas.addEventListener("click", function(e) { checkForClickableItems(e, whichRoom=currentRoom) });
 
-    btn_yes.addEventListener("click", function(){ setAction("yes") })
-    btn_no.addEventListener("click", function(){ setAction("no") })
+    btn_move.addEventListener("click",  function(){ setCurrentAction("move") });
+    btn_look.addEventListener("click",  function(){ setCurrentAction("look") });
+    btn_take.addEventListener("click",  function(){ setCurrentAction("take") });
+    btn_use.addEventListener("click",   function(){ setCurrentAction("use") });
+    btn_drop.addEventListener("click",  function(){ setCurrentAction("drop") });
+    btn_speak.addEventListener("click", function(){ setCurrentAction("speak") });
+    btn_yes.addEventListener("click",   function(){ setCurrentAction("yes") });
+    btn_no.addEventListener("click",    function(){ setCurrentAction("no") });
 
     colorRect(0, 0, canvas.width, canvas.height, 'blue'); // startup page
     colorText("Loading Images... please wait", 400, 300, 'black');
 
-    console.log(" before: loadImages ")
     loadImages(); // Once images are loaded, imageLoadingDoneSoStartGame() is called to setup the rest.
 };
 
-
 function imageLoadingDoneSoStartGame() {
-  setInterval(updateAll, 1000 / FRAMES_PER_SECOND);
-  
+  setInterval(frame, 1000 / FRAMES_PER_SECOND);
+}
+
+function frame() {
+    updateAll();
+    drawAll();
 }
 
 function updateAll() {
         drawAll();
-    updateStates();
 }
-
 
 function drawAll() {
     context.drawImage(firstRoomPic, 0,0, canvas.width,canvas.height);
     if (currentRoom === "room02") {
         context.drawImage(room2Pic, 0,0, canvas.width,canvas.height);
     }
-
 }
 
-function updateStates() {
-    
+function onTransport() {
+    loadTheNextRoom();
 }
+
 
--- js/stores/rooms.js
+++ js/stores/rooms.js
@@ -1,76 +1,89 @@
 
 
 
-function Room() {
-	this.doors = {};
-	this.itemsOnGround = {};
-}
+let Room01 = { 
 
-function Door() {
-	this.name = name;
-	this.description = description;
-	this.isOpen = false;
-	this.isLocked = isLocked;
-	this.isSecret = isSecret;
-	this.key = key;
-	this.transport = {};
-
-	
-
-}
-
-
-
-let room01 = {
-
-	name: "Dank Entry",
+	description: " ",
 	
 	allItems: {
-		stone: {},
-		lamp: {},
-	},
+		stone01: {
+			description: "A stone small enough to pocket, but large enough to be useful.",
+			coords: [319,281, 336,294],
+		},
 
-	allDoors: {
+		stone02: {
+			description: "A stone small enough to pocket, but large enough to be useful.",
+			coords: [241,325, 264,349],
+		},
+
+		torch01: {
+			description: "A torch lights the room, maybe you should take it.",
+			coords: [524,162, 557,182],
+		},
+
+		torch02: {
+			description: "A torch lights the room, maybe you should take it.",
+			coords: [241,161, 262,182],
+		},
+
+		clayPot: {
+			description: "There's strange writing on the surface. Who knows if it would be useful...",
+			coords: [86,285, 143,355],
+		},
+
 		door01: {
-			name: "busted-up door",
-			description: " ",
-
-			isOpen: 	true,
-			isLocked: 	false,
-			isSecret: 	false,
-			
+			description: "A strong wooden door with a ring hold and a keyhole.",
+			coords: [360,128, 425,240],	
+			isDoor: 	true,
 			doorKey: 	null,
-			transport: {nextRoomName:"room02", filePath:"img/room02.png" }
+			nextRoom: {nextRoomName:"room02", filePath:"img/room02.png" }
+		},
+	},
+}
+
+Room02 = {
+	description: "You walk through the door and immediately feel a menacing presence. The air feels as if it's full of static electricity and it's ice cold in here...",
+
+	allItems: {
+		creeper: {
+			description: "a creeper creeps around a doorway...",
+			coords: [419,263, 454,375],
+		},
+
+		table: {
+			description: "You examine the wooden table... You find a screwdriver taped to the underside.",
+			coords: [58,328, 201,475],
+		},
+
+		greenSplatter: {
+			description: "A green fluid has splattered across the floor and on the wall... It seems to pulsate...",
+			coords: [562,444, 630,594],
+		}
+
+		door01: {
+			description: " Wooden door stands at the far end of the room. The creeper keeps you from approaching with it's frightening presence.",
+			coords: [ 218, 225, 276, 339],
+			isOpen: false,
+		},
+
+		farOpening: {
+			description: "The doorway is guarded by a dark entity sporting a menacing pair of red eyes... It just stares at you, freezing you in your tracks.",
+			coords: [ 386, 214, 444, 399],
+			isOpen: false,
+
+		},
+
+		nearOpening {
+			description: "This doorway is free of creeping entities. It looks like to safest way forward...",
+			coords: [ 612, 198, 656, 544],
+			isOpen: true,
 		},
 
 
-		door02: {
-			name: "busted-up door",
-			description: " ",
-
-
-			isSecret: true,
-			isLocked: true,
-
-			doorKey: "simple key",
-			transport: "room-03",
-
-		}, 
 	}
+
 }
 
-// Array of all entities/clickable objects
-let entityPositionList = [
-		{ name:"Wooden Door", 	entity: room01.allDoors.door01, coords: [360,128, 425,240] },
-		{ name:"Stone-01", 			entity: room01.allItems.stone, coords: [319,281, 336,294] },
-		{ name:"Stone-02", 			entity: room01.allItems.stone, coords: [241,325, 264,349] },
-		{ name:"Clay Pot", 			entity: room01.allItems.stone, coords: [86,285, 143,355] },
-		{ name:"Left Torch", 			entity: room01.allItems.stone, coords: [241,161, 262,182] },
-		{ name:"Right Torch", 			entity: room01.allItems.stone, coords: [524,162, 557,182] },
-		
-	];
 
+let playerInventory = {};
 
-function sceneManager() {
-	if(action === "move") {}
-}
