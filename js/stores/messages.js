72896cb973f4df7c141cc901858804a9b78f8f12
J Patrick McKeown <jpmckeown@users.noreply.github.com>
2023-05-03 10:56:00 +0100

    two extra messages

--- js/stores/messages.js
+++ js/stores/messages.js
@@ -2,7 +2,7 @@ let Messages = {
 	
 	moveThroughUnopenDoor: [
 		"FOOL! You can't move through an unopened door...",
-		"Unfortunatly, you can't move through an unopened door...",
+		"Unfortunately, you can't move through an unopened door...",
 		"Try opening the door first, FOOL!",
 		"As a corporeal, solid being, you cannot move through an unopened door...",
 	],
@@ -11,12 +11,14 @@ let Messages = {
 		"You cannot do that, you FOOL!",
 		"Try something else.",
 		"Nope.",
-		"I'm afraid I can't do that, player."
+      "I'm afraid I can't do that, player.",
+      "That action is not appropriate."
 	],
 
 	cannotGoBack: [
 		"There's no more going back...",
-		"You can't go back any longer...",
+      "You can't go back any longer...",
+      "Going further back is not possible..."
 	]
 
 }
