355f01db0cee7b098c04180fe66a0beefb14cee1
Christer Kaitila <breakdance@mcfunkypants.com>
2023-06-23 10:08:47 -0700

    minor tweaks to ui

--- js/stores/messages.js
+++ js/stores/messages.js
@@ -1,24 +1,28 @@
 let Messages = { 
 	
 	moveThroughUnopenDoor: [
-		"FOOL! You can't move through an unopened door...",
+		"The door is still closed!",
+		"You can't move through an unopened door...",
 		"Unfortunately, you can't move through an unopened door...",
-		"Try opening the door first, FOOL!",
+		"Try opening the door first!",
 		"As a corporeal, solid being, you cannot move through an unopened door...",
 	],
 
 	cannotTakeAction: [
-		"You cannot do that, you FOOL!",
+		"That didn't work.",
+		"Nothing happened.",
+		"Sorry, that doesn't work.",
+		"You cannot do that.",
 		"Try something else.",
 		"Nope.",
-      "I'm afraid I can't do that, player.",
-      "That action is not appropriate."
+        "I'm afraid I can't do that, player.",
+        "That action is not appropriate."
 	],
 
 	cannotGoBack: [
-		"There's no more going back...",
-      "You can't go back any longer...",
-      "Going further back is not possible..."
+	    "There's no more going back...",
+        "You can't go back any longer...",
+        "Going further back is not possible..."
 	]
 
 }
